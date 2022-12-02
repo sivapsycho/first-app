6f1b10ba250dcad2a5552692db251850d3579f05
sivaphyco <narayanakanaparthi9@gmail.com>
2022-12-02 16:51:09 +0530

    map

--- react-props-main/src/Events.js
+++ react-props-main/src/Events.js
@@ -1,20 +1,24 @@
 import {Component} from 'react'
 class Events extends Component{
 	state={
-		username:""
+		username:"",
+		password:""
 	}
 	handleClick=()=>{
 		console.log(this.state.username)
+		console.log(this.state.password)
 	}
 	handleChange=(e)=>{
 		this.setState({
-			[e.target.name]:e.target.name
+			[e.target.name]:e.target.value,
+			
 		})
 	}
 	render(){
 		return(
 			<div>
-			<input onChange={this.handleChange}placeholder="Enter Username"/>
+			<input name="username" onChange={this.handleChange}placeholder="Enter Username"/>
+			<input name="password" onChange={this.handleChange}placeholder="Enter Password"/>
 			<button onClick={this.handleClick}>submit</button>
 			</div>
 		)
